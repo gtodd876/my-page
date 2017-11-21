6b75f5186456151b473e980519efd32972a8d90f
Todd Matthews <gtmatthews@gmail.com>
2017-11-21 06:33:27 -0500

    Add websocketserver

--- server.js
+++ server.js
@@ -1,12 +1,71 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const WebSocketServer = require('ws').Server;
+const server = require('http').createServer();
+const exec = require('child_process').exec;
+const wss = new WebSocketServer({ server: server });
+
+const PORT = 3001;
 
 app.use('/public', express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
   res.sendFile('index.html', {root: __dirname});
 });
 
-app.listen(3001, () => {
-  console.log('http://localhost:3001');
+let pushDataInterval;
+
+wss.on('connection', function connection(ws) {
+   console.log('clients connected: ', wss.clients.size);
+
+  if (!pushDataInterval) {
+    pushDataInterval = getServerLoad();
+  }
+
+  if (ws.readyState === ws.OPEN) {
+    ws.send('welcome!');
+  }
+
+  ws.on('close', function close() {
+    if (wss.clients.size === 0) {
+      clearInterval(pushDataInterval);
+      pushDataInterval = null;
+    }
+    console.log('disconnected');
+  });
+
+  ws.on('error', function error() {
+    console.log('error');
+  });
 });
+
+/**
+ * Run shell script on an interval and broadcast to connected clients.
+ * @return {Object} The interval object
+ */
+function getServerLoad() {
+  return setInterval(() => {
+    // Execute bash script
+    const loadScript = exec(`./proc.sh`);
+
+    loadScript.stdout.on('data', function(data) {
+      wss.broadcast(JSON.stringify({name: 'load', data}));
+    });
+
+  }, 2 * 1000);
+}
+
+/**
+ * Broadcast data to all connected clients
+ * @param  {Object} data
+ * @void
+ */
+wss.broadcast = function broadcast(data) {
+  console.log('Broadcasting: ', data);
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
+};
+
+server.on('request', app);
+server.listen(PORT, function () { console.log('Listening on ' + PORT); });
