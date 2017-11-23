7e218e7cfcec818df979166d593560330d6b019a
Todd Matthews <gtmatthews@gmail.com>
2017-11-23 07:04:43 -0500

    Add nodeMailer to contact form

--- server.js
+++ server.js
@@ -1,13 +1,50 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-// const WebSocketServer = require('ws').Server;
-// const server = require('http').createServer();
-// const exec = require('child_process').exec;
-// const wss = new WebSocketServer({ server: server });
+const bodyParser = require('body-parser')
+const nodemailer = require('nodemailer');
 
-const PORT = 3001;
+app.use(bodyParser.urlencoded({ extended: false }));
+// NODE MAILER
+app.post('/contact', (req, res) => {
+  console.log('It worked');
+  console.log(req.body);
+  let transporter = nodemailer.createTransport(
+    {
+      host: 'smtp.gmail.com',
+      port:587,
+      secure:false,
+      auth: {
+        user: "toddmatthewsme@gmail.com",
+        pass: "0dd0dd76"
+      }
+    }
+  );
+  let saveData = req.body;
+  const html = `<html>
+                  <body style="font-family:Arial;">
+                    <p>Message from ${saveData.name} at ${saveData.email}</p>
+                    <p>${saveData.message}</p>
+                  </body>
+                </html>`;
+  
+  let mailOptions = {
+    to: 'gtmatthews@gmail.com',
+    subject: 'nodemailer contact form',
+    html: html
+  };
 
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return console.log(error);
+    }
+    console.log('Message sent: %s', info.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+  });
+  res.sendFile('success.html', {root: __dirname});
+});
+
+// ROUTES
 app.use('/public', express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
   res.sendFile('index.html', {root: __dirname});
@@ -23,58 +60,4 @@ app.get('/portfolio', (req, res) => {
 });
 
 app.listen(3001, () => console.log('Server listening at http://127.0.0.1:3001 !'))
-
-// let pushDataInterval;
-
-// wss.on('connection', function connection(ws) {
-//    console.log('clients connected: ', wss.clients.size);
-
-//   if (!pushDataInterval) {
-//     pushDataInterval = getServerLoad();
-//   }
-
-//   if (ws.readyState === ws.OPEN) {
-//     ws.send('welcome!');
-//   }
-
-//   ws.on('close', function close() {
-//     if (wss.clients.size === 0) {
-//       clearInterval(pushDataInterval);
-//       pushDataInterval = null;
-//     }
-//     console.log('disconnected');
-//   });
-
-//   ws.on('error', function error() {
-//     console.log('error');
-//   });
-// });
-
-// /**
-//  * Run shell script on an interval and broadcast to connected clients.
-//  * @return {Object} The interval object
-//  */
-// function getServerLoad() {
-//   return setInterval(() => {
-//     // Execute bash script
-//     const loadScript = exec(`./proc.sh`);
-
-//     loadScript.stdout.on('data', function(data) {
-//       wss.broadcast(JSON.stringify({name: 'load', data}));
-//     });
-
-//   }, 2 * 1000);
-// }
-
-// /**
-//  * Broadcast data to all connected clients
-//  * @param  {Object} data
-//  * @void
-//  */
-// wss.broadcast = function broadcast(data) {
-//   console.log('Broadcasting: ', data);
-//   wss.clients.forEach(function each(client) {
-//     client.send(data);
-//   });
-// };
 
